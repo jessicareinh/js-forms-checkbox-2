f1e443c589972dd6dedc52462ace2c518a474645
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2023-12-12 22:45:54 +0100

    add solutions js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,9 +17,33 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+const formData = new FormData(event.target);
+const data = Object.fromEntries(formData);
+
+console.log(data);
+
+if (!tosCheckbox.checked) {
+  showTosError();
+  successMessage.setAttribute("hidden", "");
+  return;
+} else {
+  hideTosError();
+  successMessage.removeAttribute("hidden");
+
+  alert("Firm submitted");
+});
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
+
   alert("Form submitted");
+});
+
+tosCheckbox.addEventListener("change"; () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+} else {
+  showTosError();
+}
 });
